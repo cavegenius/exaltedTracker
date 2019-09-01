ca028ae648f17cf15e5e1720f5ecb52d1197b759
Wyatt <>
2019-09-01 17:07:33 -0400

    fixed 2 bugs with merits

--- app/Http/Controllers/MeritController.php
+++ app/Http/Controllers/MeritController.php
@@ -19,10 +19,11 @@ class MeritController extends Controller
             } else if($key[0] == 'i') {
                 $saveData[$key[2]]['id'] = $value;
             } else {
-                $saveData[$key[0]]['value'] = substr($key, -1);
+                $number = explode ("v", $key);  
+                $saveData[$number[0]]['value'] = substr($key, -1);
             }
         }
-        
+
         foreach($saveData as $merit) {
             if(array_key_exists('id', $merit)) {
                 $meritModel = Merit::firstOrNew(['id' => $merit['id']]);
@@ -31,7 +32,11 @@ class MeritController extends Controller
             }
             $meritModel->characterId = $character;
             $meritModel->name = $merit['name'];
-            $meritModel->value = $merit['value'];
+            if (array_key_exists('value',$merit)) {
+                $meritModel->value = $merit['value'];
+            } else {
+                $meritModel->value = '0';
+            }
             $meritModel->save();
         }
         return true;
